3fbf51827a41cefb00dd509f86cc91aef863cc89
akshay <akshaymishra5395@gmail.com>
2019-10-17 15:58:59 +0530

    Update check_viewset_.py

--- check_viewset_.py
+++ check_viewset_.py
@@ -1,18 +1,51 @@
-<--------------------------------------------- .Serializer----------------------------------------------------->
+#<--------------------------------------------- .Serializer----------------------------------------------------->
 
 class EmpoyeeSerializer(serializer.HyperlinkModelSerializer):
+  class Meta:
+    fields=(
+      'first_name',
+      'last_name',
+      'email',
+      'url')
+    
+# class EmpoyeeSerializer(serializer.ModelSerializer):
+#   class Meta:
+#     #fields='__all__'
+#     fields=(
+#       'first_name',
+#       'last_name',
+#       'email',
+#       'url')
 
+# class EmpoyeeSerializer(serializer.Serializer):
+Need to be edited
+#   class Meta:
+#     #fields='__all__'
+#     fields=(
+#       'first_name',
+#       'last_name',
+#       'email',
+#       'url')
 
+#<----------------------------------------------   .views -------------------------------------------------------->
+from rest_framework import viewsets
+from .serializers import EmpoyeeSerializer
 
-<----------------------------------------------   .views -------------------------------------------------------->
+class EmployeeViewset(viewsets.ModelViewset):
+  queryset = User.objects.all()
+  serializer_class = EmployeeSerializer
+  
 
+#<----------------------------------------------   Project urls -------------------------------------------------------->
+urlpatterns = [
+  path('api/v1/',include('employee.api_urls'))
+]
+#<----------------------------------------------   .api_urls ----------------------------------------------------------->
+from rest_framework import routers
 
+routes=routers.DefaultRouter()
+router.register('',EmployeeViewset)
 
-
-
-
-
-
-
-<----------------------------------------------   .views -------------------------------------------------------->
-
+urlpatterns = [
+  path('employee',include('router.urls'))
+]
